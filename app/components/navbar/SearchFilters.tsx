33b545f70431e364ef7a8fd849f55bc5c1572609
Emmanuel-Ejeagha <nuel6700@gmail.com>
2024-08-06 16:30:51 +0100

    SearchFilters.tsx

---
+++ app/components/navbar/SearchFilters.tsx
@@ -0,0 +1,35 @@
+import Image from "next/image";
+
+const SearchFilters = () => {
+  return (
+    <div className="h-[64px] flex flex-row items-center justify-between border rounded-full">
+      <div className="hidden lg:block">
+        <div className="flex flex-row items-center justify-between">
+          <div className="w-[250px] cursor-pointer h-[64px] px-8 flex flex-col justify-center rounded-full hover:bg-gray-100">
+            <p className="text-xs font-semibold">Where</p>
+            <p className="text-sm">Wanted location</p>
+          </div>
+          <div className="cursor-pointer h-[64px] px-8 flex flex-col justify-center rounded-full hover:bg-gray-100">
+            <p className="text-xs font-semibold">Check in</p>
+            <p className="text-sm">Add dates</p>
+          </div>
+          <div className="cursor-pointer h-[64px] px-8 flex flex-col justify-center rounded-full hover:bg-gray-100">
+            <p className="text-xs font-semibold">Check out</p>
+            <p className="text-sm">Add dates</p>
+          </div>
+          <div className="cursor-pointer h-[64px] px-8 flex flex-col justify-center rounded-full hover:bg-gray-100">
+            <p className="text-xs font-semibold">Who</p>
+            <p className="text-sm">Add guests</p>
+          </div>
+        </div>
+      </div>
+      <div className="p-2">
+        <div className="cursor-pointer p-4 bg-airbnb hover:bg-airbnb-dark transition rounded-full text-white">
+          <Image src="/search.svg" alt="search icon" width={16} height={16} />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SearchFilters;
