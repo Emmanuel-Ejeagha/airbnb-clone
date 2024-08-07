d6cbb9c92d84df94e6ad53cc1314c0f897a81c37
Emmanuel-Ejeagha <nuel6700@gmail.com>
2024-08-07 20:42:07 +0100

    ContactButton.tsx

---
+++ app/components/ContactButton.tsx
@@ -0,0 +1,9 @@
+const ContactButton = () => {
+  return (
+    <div className="py-3 mt-5 cursor-pointer px-5 bg-airbnb text-white rounded-xl hover:bg-airbnb-dark transition">
+      Contact
+    </div>
+  );
+};
+
+export default ContactButton;
