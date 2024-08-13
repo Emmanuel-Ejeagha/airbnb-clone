e20d9ae8d4f37093d7b88d3176cb935beade0fab
Emmanuel-Ejeagha <nuel6700@gmail.com>
2024-08-13 23:56:53 +0100

    modified

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,8 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "./components/navbar/Navbar";
-// import LoginModal from "./components/modals/LoginModal";
+import LoginModal from "./components/modals/LoginModal";
+import SignupModal from "./components/modals/SignupModal";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -28,6 +29,8 @@ export default function RootLayout({
       <body className={inter.className}>
         <Navbar />
         <div className="pt-32">{children}</div>
+        <LoginModal />
+        <SignupModal />
       </body>
     </html>
   );
