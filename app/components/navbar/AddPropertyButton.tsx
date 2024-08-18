5e69e09a07714d29b0b3a7513c83449a8649bb81
Emmanuel-Ejeagha <nuel6700@gmail.com>
2024-08-18 23:36:52 +0100

    modified

--- app/components/navbar/AddPropertyButton.tsx
+++ app/components/navbar/AddPropertyButton.tsx
@@ -1,7 +1,7 @@
 const AddPropertyButton = () => {
   return (
     <div className="p-2 cursor-pointer text-sm font-semibold rounded-full hover:bg-gray-200">
-      MyBnB your home
+      MyAirBnB Your Home
     </div>
   );
 };
