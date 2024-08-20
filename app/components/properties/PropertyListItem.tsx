ad2935ddf43d62ee3f139bc4e7ee45c662cccf66
Emmanuel-Ejeagha <nuel6700@gmail.com>
2024-08-20 23:09:27 +0100

    changed file name

---
+++ app/components/properties/PropertyListItem.tsx
@@ -0,0 +1,27 @@
+import Image from "next/image";
+
+const PropertyListItem = () => {
+  return (
+    <div className="cursor-pointer">
+      <div className="relative overflow-hidden aspect-square rounded-xl">
+        <Image
+          fill
+          src="/beachhouse.jpg"
+          alt="beach house"
+          sizes="(max-width: 768px) 768px, (max-width: 1200px): 768px, 768px"
+          className="hover:scale-110 object-cover transition h-full w-full"
+        />
+      </div>
+      <div className="mt-2">
+        <p className="text-lg font-bold">Property Name</p>
+      </div>
+      <div className="mt-2">
+        <p className="text-sm">
+          <strong className="text-sm text-gray-500">₦20000 </strong>per night
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default PropertyListItem;
