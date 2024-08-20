6cb24f823c8c46e02219b1aaeec3431b39baf84e
Emmanuel-Ejeagha <nuel6700@gmail.com>
2024-08-20 23:10:17 +0100

    modified

--- app/components/properties/PropertList.tsx
+++ app/components/properties/PropertList.tsx
@@ -1,11 +1,42 @@
-import PropertyListitem from "./PropertyListitem";
+"use client";
+
+import { useEffect, useState } from "react";
+import PropertyListItem from "./PropertyListItem";
+
+export type PropertyType = {
+  id: string;
+}
 
 const PropertyList = () => {
+  const [properties, setProperties] = useState<PropertyType[]>([]);
+  const getProperties = async () => {
+    const url = "http://localhost:8000/api/properties/";
+
+    await fetch(url, {
+      method: "GET",
+    })
+      .then((response) => response.json())
+      .then((json) => {
+        console.log("json", json);
+
+        setProperties(json.data);
+      })
+      .catch((error) => {
+        console.log("error", error);
+      });
+  };
+  useEffect(() => {
+    getProperties();
+  }, []);
+
   return (
     <>
-      <PropertyListitem />
-      <PropertyListitem />
-      <PropertyListitem />
+      {properties.map((property) => {
+        return (
+      <PropertyListItem />
+          key={property.id}
+        )
+      })}
     </>
   );
 };
