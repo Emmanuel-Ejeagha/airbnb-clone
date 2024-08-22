e702d7c2d79631ccbd556c89b2998dcc9f6fd346
Emmanuel-Ejeagha <nuel6700@gmail.com>
2024-08-22 23:07:15 +0100

    Modified:

--- app/components/modals/SignupModal.tsx
+++ app/components/modals/SignupModal.tsx
@@ -6,6 +6,7 @@ import { useRouter } from "next/navigation";
 import CustomButton from "../forms/CustomButton";
 import useSignupModal from "@/app/hooks/useSignupModal";
 import apiService from "@/app/services/apiService";
+import { handleLogin } from "@/app/lib/actions";
 
 const SignupModal = () => {
   // Variables
@@ -22,18 +23,19 @@ const SignupModal = () => {
       email: email,
       password1: password1,
       password2: password2,
-    };
-    const response = await apiService.post("/api/auth/register/", formData);
+    }
+    const response = await apiService.post("/api/auth/register/", formData)
 
     if (response.access) {
-      //handleLogin
+      handleLogin(response.user.pk, response.access, response.refresh);
+
       signupModal.close();
 
       router.push("/");
     } else {
       const tmpErrors: string[] = Object.values(response).map((error: any) => {
         return error;
-      });
+      })
       setErrors(tmpErrors);
     }
   };
@@ -47,7 +49,7 @@ const SignupModal = () => {
           className="w-full h-[54px] px-4 border border-gray-300 rounded-xl "
         />
         <input
-          onChange={(e) => setPassword2(e.target.value)}
+          onChange={(e) => setPassword1(e.target.value)}
           placeholder="Your password"
           type="password"
           className="w-full h-[54px] px-4 border border-gray-300 rounded-xl "
