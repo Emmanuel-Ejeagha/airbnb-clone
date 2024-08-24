c6df260af150f5599585db5c5603e2e7c5ab0f9c
Emmanuel-Ejeagha <nuel6700@gmail.com>
2024-08-24 21:51:28 +0100

    Added: property button

--- app/components/navbar/AddPropertyButton.tsx
+++ app/components/navbar/AddPropertyButton.tsx
@@ -1,7 +1,26 @@
+"use client";
+
+import useLoginModal from "@/app/hooks/useLoginModal";
 import useAddPropertyModal from "@/app/hooks/useAddPropertyModal";
-const AddPropertyButton = () => {
+
+interface AddPropertyButtonProps {
+  userId?: string | null;
+}
+const AddPropertyButton: React.FC<AddPropertyButtonProps> = ({ userId }) => {
+  const loginModal = useLoginModal();
+  const addPropertyModal = useAddPropertyModal();
+  const airbnbYourHome = () => {
+    if (userId) {
+      addPropertyModal.open();
+    } else {
+      loginModal.open();
+    }
+  };
   return (
-    <div className="p-2 cursor-pointer text-sm font-semibold rounded-full hover:bg-gray-200">
+    <div
+      onClick={airbnbYourHome}
+      className="p-2 cursor-pointer text-sm font-semibold rounded-full hover:bg-gray-200"
+    >
       MyAirBnB Your Home
     </div>
   );
