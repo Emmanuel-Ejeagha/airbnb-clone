d0d64e9244b134eaacfd399749fd895fba1f9053
Emmanuel-Ejeagha <nuel6700@gmail.com>
2024-08-26 20:08:02 +0100

    modified

--- app/components/addproperty/Categories.tsx
+++ app/components/addproperty/Categories.tsx
@@ -14,21 +14,25 @@ const Categories: React.FC<CategoriesProps> = ({
       <div className="pt-3 cursor-pointer pb-6 flex item-center space-x-12">
         <div
           onClick={() => setCategory("Icons")}
-          className="pb-4 flex flex-col items-center space-y-2 border-b-2 border-black opacity-60 hover:opacity-100 hover:border-gray-200"
+ className={`pb-4 flex flex-col items-center space-y-2 border-b-2 ${
+            dataCategory == "Icons" ? "border-gray-800" : "border-white"
+          } opacity-60 hover:opacity-100 hover:border-gray-200`}
         >
           <Image src="/icons.png" alt="icon logo" width={20} height={20} />
           <span className="text-xs">Icons</span>
         </div>
         <div
           onClick={() => setCategory("Island")}
-          className="pb-4 flex flex-col items-center space-y-2 border-b-2 border-white opacity-60 hover:opacity-100 hover:border-gray-200"
+  className={`pb-4 flex flex-col items-center space-y-2 border-b-2 ${
+            dataCategory == "Island" ? "border-gray-800" : "border-white"
+          } opacity-60 hover:opacity-100 hover:border-gray-200`}
         >
           <Image src="/island.jpg" alt="icon logo" width={20} height={20} />
           <span className="text-xs">Island</span>
         </div>
         <div
           onClick={() => setCategory("Beach")}
-          className={`pb-4 flex flex-col items-center space-y-2 border-b-2 ${
+className={`pb-4 flex flex-col items-center space-y-2 border-b-2 ${
             dataCategory == "Beach" ? "border-gray-800" : "border-white"
           } opacity-60 hover:opacity-100 hover:border-gray-200`}
         >
@@ -37,22 +41,28 @@ const Categories: React.FC<CategoriesProps> = ({
         </div>
         <div
           onClick={() => setCategory("Houseboats")}
-          className="pb-4 flex flex-col items-center space-y-2 border-b-2 border-white opacity-60 hover:opacity-100 hover:border-gray-200"
-        >
+className={`pb-4 flex flex-col items-center space-y-2 border-b-2 ${
+            dataCategory == "Houseboats" ? "border-gray-800" : "border-white"
+          } opacity-60 hover:opacity-100 hover:border-gray-200`}
+        >        
           <Image src="/boathouse.jpg" alt="icon logo" width={20} height={20} />
           <span className="text-xs">Houseboats</span>
         </div>
         <div
           onClick={() => setCategory("Cabins")}
-          className="pb-4 flex flex-col items-center space-y-2 border-b-2 border-white opacity-60 hover:opacity-100 hover:border-gray-200"
+className={`pb-4 flex flex-col items-center space-y-2 border-b-2 ${
+            dataCategory == "Cabins" ? "border-gray-800" : "border-white"
+          } opacity-60 hover:opacity-100 hover:border-gray-200`}
         >
           <Image src="/cabins.jpg" alt="icon logo" width={20} height={20} />
           <span className="text-xs">Cabins</span>
         </div>
         <div
           onClick={() => setCategory("Tiny homes")}
-          className="pb-4 flex flex-col items-center space-y-2 border-b-2 border-white opacity-60 hover:opacity-100 hover:border-gray-200"
-        >
+className={`pb-4 flex flex-col items-center space-y-2 border-b-2 ${
+            dataCategory == "Tiny homes" ? "border-gray-800" : "border-white"
+          } opacity-60 hover:opacity-100 hover:border-gray-200`}
+        >        
           <Image src="/tinyhomes.jpg" alt="icon logo" width={20} height={20} />
           <span className="text-xs">Tiny homes</span>
         </div>
