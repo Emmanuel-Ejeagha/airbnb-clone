8a9d135d1957a7615bb50b81a6dabc4a7a7ad902
Emmanuel-Ejeagha <nuel6700@gmail.com>
2024-08-28 17:24:37 +0100

    Modified: api/prperies

--- app/components/modals/SignupModal.tsx
+++ app/components/modals/SignupModal.tsx
@@ -23,8 +23,11 @@ const SignupModal = () => {
       email: email,
       password1: password1,
       password2: password2,
-    }
-    const response = await apiService.post("/api/auth/register/", formData)
+    };
+    const response = await apiService.postWithoutToken(
+      "/api/auth/register/",
+      JSON.stringify(formData)
+    );
 
     if (response.access) {
       handleLogin(response.user.pk, response.access, response.refresh);
@@ -35,7 +38,7 @@ const SignupModal = () => {
     } else {
       const tmpErrors: string[] = Object.values(response).map((error: any) => {
         return error;
-      })
+      });
       setErrors(tmpErrors);
     }
   };
