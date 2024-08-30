9e08a86c742d2f39f107dc37779324c5f5ab5253
Emmanuel-Ejeagha <nuel6700@gmail.com>
2024-08-30 20:04:52 +0100

    modified

--- app/properties/[id]/page.tsx
+++ app/properties/[id]/page.tsx
@@ -3,7 +3,7 @@ import ReservationSidebar from "@/app/components/properties/ReservationSidebar";
 import apiService from "@/app/services/apiService";
 
 const PropertyDetailPage = async ({ params }: { params: { id: string } }) => {
-  const property = await apiService.get;
+  const property = await apiService.get(`/api/properties/${params.id}`);
   return (
     <main className="max-w-[1500px] mx-auto px-6 pb-6">
       <div className="mb-4 w-full h-[64vh] rounded-xl overflow-hidden relative">
@@ -16,9 +16,10 @@ const PropertyDetailPage = async ({ params }: { params: { id: string } }) => {
       </div>
       <div className=" grid grid-cols-1 md:grid-cols-5 gap-4">
         <div className="col-span-3 py-6 pr-6">
-          <h1 className="mb-4 text-4xl">Property name</h1>
+          <h1 className="mb-4 text-4xl">{property.title}</h1>
           <span className="mb-6 block text-lg text-gray-600">
-            4 guests -2 bedrooms - 1 bathroom
+            {property.guests} guests - {property.bedrooms} bedrooms -{" "}
+            {property.bathroom} bathroom
           </span>
           <hr />
           <div className="py-6 flex items-center space-x-4">
@@ -34,12 +35,7 @@ const PropertyDetailPage = async ({ params }: { params: { id: string } }) => {
             </p>
           </div>
           <hr />
-          <p className="mt-6 text-lg">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab optio
-            omnis, animi eius, iusto sequi odit voluptatum molestias ratione
-            dolorum debitis adipisci ipsum blanditiis maxime, earum cumque qui
-            rem aspernatur.
-          </p>
+          <p className="mt-6 text-lg">{property.description}</p>
         </div>
         <ReservationSidebar />
       </div>
