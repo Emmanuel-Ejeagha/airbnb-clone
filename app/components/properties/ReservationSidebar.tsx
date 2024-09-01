de5284068fea5143b35ab9cbf4749978263f1eeb
Emmanuel-Ejeagha <nuel6700@gmail.com>
2024-09-01 04:08:04 +0100

    Added: calculator

--- app/components/properties/ReservationSidebar.tsx
+++ app/components/properties/ReservationSidebar.tsx
@@ -1,14 +1,61 @@
+"use client";
+
+import { useState, useEffect } from "react";
+import { Range } from "react-date-range";
+import { differenceInDays, eachDayOfInterval } from "date-fns";
+
+import apiService from "@/app/services/apiService";
+import useLoginModal from "@/app/hooks/useLoginModal";
+
+const initialDateRange = {
+  startDate: new Date(),
+  endDate: new Date(),
+  key: "selection",
+};
+
 export type Property = {
   id: string;
+  guests: number;
   price_per_night: number;
 };
 
 interface ReservationSidebarProps {
+  userId: string | null;
   property: Property;
 }
 const ReservationSidebar: React.FC<ReservationSidebarProps> = ({
   property,
+  userId,
 }) => {
+  const loginModal = useLoginModal();
+  const [fee, setFee] = useState<number>(0);
+  const [nights, setNights] = useState<number>(1);
+  const [totalPrice, setTotalPrice] = useState<number>(0);
+  const [dateRange, setDateRange] = useState<Range>(initialDateRange);
+  const [minDate, setMinDate] = useState<Date>(new Date());
+  const [guests, setGuests] = useState<string>("1");
+  const guestsRange = Array.from(
+    { length: property.guests },
+    (_, index) => index + 1
+  );
+
+  useEffect(() => {
+    if (dateRange.startDate && dateRange.endDate) {
+      const dayCount = differenceInDays(dateRange.endDate, dateRange.startDate);
+      if (dayCount && property.price_per_night) {
+        const _fee = ((dayCount * property.price_per_night) / 100) * 5;
+        setFee(_fee);
+        setTotalPrice(dayCount * property.price_per_night + _fee);
+        setNights(dayCount);
+      } else {
+        const _fee = (property.price_per_night / 100) * 5;
+        setFee(_fee);
+        setTotalPrice(property.price_per_night * _fee);
+        setNights(1);
+      }
+    }
+  }, [dateRange]);
+
   return (
     <aside className="p-6 col-span-2 rounded-xl border border-gray-300 shadow-xl mt-6">
       <h2 className="mb-5 text-2xl">${property.price_per_night} per night </h2>
@@ -16,28 +63,35 @@ const ReservationSidebar: React.FC<ReservationSidebarProps> = ({
         <label htmlFor="" className="mb-2 block font-bold text-xs">
           Guests
         </label>
-        <select name="" id="" className="w-full -ml-1 text-xm">
-          <option value="">1</option>
-          <option value="">2</option>
-          <option value="">3</option>
-          <option value="">4</option>
+        <select
+          onChange={(e) => setGuests(e.target.value)}
+          value={guests}
+          className="w-full -ml-1 text-xm"
+        >
+          {guestsRange.map((number) => (
+            <option key={number} value={number}>
+              (number)
+            </option>
+          ))}
         </select>
       </div>
       <div className="w-full mb-6 py-6 text-center text-white bg-airbnb rounded-xl hover:bg-airbnb-dark">
         Book
       </div>
       <div className="mb-4 flex justify-between align-center">
-        <p>₦200 * 4 nights</p>
-        <p>₦800,000</p>
+        <p>
+          ₦{property.price_per_night} * {nights} nights
+        </p>
+        <p>₦{property.price_per_night * nights}</p>
       </div>
       <div className="mb-4 flex justify-between align-center">
         <p>MyBnB fee</p>
-        <p>₦40,000</p>
+        <p>₦{fee}</p>
       </div>
       <hr />
       <div className="mt-4 flex justify-between align-center font-bold">
         <p>Total </p>
-        <p> ₦840,000</p>
+        <p>₦{totalPrice}</p>
       </div>
     </aside>
   );
