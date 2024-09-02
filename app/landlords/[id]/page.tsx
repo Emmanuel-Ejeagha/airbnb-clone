fff939907d8151e97fcc3a04362dbd3d1279165b
Emmanuel-Ejeagha <nuel6700@gmail.com>
2024-09-02 23:09:10 +0100

    modified

--- app/landlords/[id]/page.tsx
+++ app/landlords/[id]/page.tsx
@@ -13,19 +13,19 @@ const LandlordDetailPage = async ({ params }: { params: { id: string } }) => {
         <aside className="col-span-1 mb-4">
           <div className="flex flex-col items-center p-6 rounded-xl border border-gray-300 shadollw-xl">
             <Image
-              src="/child.jpg"
+              src={landlord.avatar_url}
               width={200}
               height={200}
               alt="landlord name"
               className="rounded-full"
             />
-            <h1 className="mt-6 text-2xl">Landlord</h1>
-            <ContactButton />
+            <h1 className="mt-6 text-2xl">{landlord.name}</h1>
+            {userId != params.id && <ContactButton />}
           </div>
         </aside>
         <div className="col-span-1 md:col-span-3 pl-0 md:pl-6">
           <div className=" grid grid-cols-1 md:grid-cols-3 gap-6">
-            <PropertyList />
+            <PropertyList landlord_id={params.id} />
           </div>
         </div>
       </div>
