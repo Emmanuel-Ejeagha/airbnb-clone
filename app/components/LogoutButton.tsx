a6f48eada919594ec35eb4cea7eae41c438e64be
Emmanuel-Ejeagha <nuel6700@gmail.com>
2024-09-09 23:04:36 +0100

    modified

--- app/components/LogoutButton.tsx
+++ app/components/LogoutButton.tsx
@@ -1,17 +1,26 @@
-"use client";
+'use client';
 
 import { useRouter } from "next/navigation";
-import { resetAuthCookies } from "../lib/actions";
+
+import { resetAuthCookies } from '../lib/actions';
+
 import MenuLink from "./navbar/MenuLink";
 
 const LogoutButton: React.FC = () => {
-  const router = useRouter();
-  const submitLogout = async () => {
-    resetAuthCookies();
-    router.push("/");
-  };
+    const router = useRouter();
 
-  return <MenuLink label="Log out" onClick={submitLogout} />;
-};
+    const submitLogout = async () => {
+        resetAuthCookies();
+
+        router.push('/')
+    }
+
+    return (
+        <MenuLink
+            label="Log out"
+            onClick={submitLogout}
+        />
+    )
+}
 
 export default LogoutButton;
