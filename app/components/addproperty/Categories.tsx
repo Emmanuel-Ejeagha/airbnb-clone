7dbbda9a39f908039a73544c3f5c7a4164f54fd9
Emmanuel-Ejeagha <nuel6700@gmail.com>
2024-09-09 23:08:22 +0100

    modified

--- app/components/addproperty/Categories.tsx
+++ app/components/addproperty/Categories.tsx
@@ -13,57 +13,66 @@ const Categories: React.FC<CategoriesProps> = ({
     <>
       <div className="pt-3 cursor-pointer pb-6 flex item-center space-x-12">
         <div
-          onClick={() => setCategory("Icons")}
- className={`pb-4 flex flex-col items-center space-y-2 border-b-2 ${
-            dataCategory == "Icons" ? "border-gray-800" : "border-white"
-          } opacity-60 hover:opacity-100 hover:border-gray-200`}
-        >
-          <Image src="/icons.png" alt="icon logo" width={20} height={20} />
-          <span className="text-xs">Icons</span>
-        </div>
-        <div
-          onClick={() => setCategory("Island")}
-  className={`pb-4 flex flex-col items-center space-y-2 border-b-2 ${
-            dataCategory == "Island" ? "border-gray-800" : "border-white"
-          } opacity-60 hover:opacity-100 hover:border-gray-200`}
-        >
-          <Image src="/island.jpg" alt="icon logo" width={20} height={20} />
-          <span className="text-xs">Island</span>
-        </div>
-        <div
-          onClick={() => setCategory("Beach")}
-className={`pb-4 flex flex-col items-center space-y-2 border-b-2 ${
+          onClick={() => setCategory("beach")}
+          className={`pb-4 flex flex-col items-center space-y-2 border-b-2 ${
             dataCategory == "Beach" ? "border-gray-800" : "border-white"
-          } opacity-60 hover:opacity-100 hover:border-gray-200`}
+          } opacity-60 hover:border-gray-200 hover:opacity-100`}
         >
-          <Image src="/beach.jpg" alt="icon logo" width={20} height={20} />
+          <Image
+            src="/beach.jpg"
+            alt="Category - Beach"
+            width={20}
+            height={20}
+          />
+
           <span className="text-xs">Beach</span>
         </div>
+
         <div
-          onClick={() => setCategory("Houseboats")}
-className={`pb-4 flex flex-col items-center space-y-2 border-b-2 ${
-            dataCategory == "Houseboats" ? "border-gray-800" : "border-white"
-          } opacity-60 hover:opacity-100 hover:border-gray-200`}
-        >        
-          <Image src="/boathouse.jpg" alt="icon logo" width={20} height={20} />
-          <span className="text-xs">Houseboats</span>
-        </div>
-        <div
-          onClick={() => setCategory("Cabins")}
-className={`pb-4 flex flex-col items-center space-y-2 border-b-2 ${
-            dataCategory == "Cabins" ? "border-gray-800" : "border-white"
-          } opacity-60 hover:opacity-100 hover:border-gray-200`}
+          onClick={() => setCategory("villas")}
+          className={`pb-4 flex flex-col items-center space-y-2 border-b-2 ${
+            dataCategory == "Villas" ? "border-gray-800" : "border-white"
+          } opacity-60 hover:border-gray-200 hover:opacity-100`}
         >
-          <Image src="/cabins.jpg" alt="icon logo" width={20} height={20} />
+          <Image
+            src="/island.jpg"
+            alt="Category - Beach"
+            width={20}
+            height={20}
+          />
+
+          <span className="text-xs">Villas</span>
+        </div>
+
+        <div
+          onClick={() => setCategory("cabins")}
+          className={`pb-4 flex flex-col items-center space-y-2 border-b-2 ${
+            dataCategory == "Cabins" ? "border-gray-800" : "border-white"
+          } opacity-60 hover:border-gray-200 hover:opacity-100`}
+        >
+          <Image
+            src="/cabins.jpg"
+            alt="Category - Beach"
+            width={20}
+            height={20}
+          />
+
           <span className="text-xs">Cabins</span>
         </div>
+
         <div
-          onClick={() => setCategory("Tiny homes")}
-className={`pb-4 flex flex-col items-center space-y-2 border-b-2 ${
+          onClick={() => setCategory("tiny_homes")}
+          className={`pb-4 flex flex-col items-center space-y-2 border-b-2 ${
             dataCategory == "Tiny homes" ? "border-gray-800" : "border-white"
-          } opacity-60 hover:opacity-100 hover:border-gray-200`}
-        >        
-          <Image src="/tinyhomes.jpg" alt="icon logo" width={20} height={20} />
+          } opacity-60 hover:border-gray-200 hover:opacity-100`}
+        >
+          <Image
+            src="/tinyhomes.jpg"
+            alt="Category - Beach"
+            width={20}
+            height={20}
+          />
+
           <span className="text-xs">Tiny homes</span>
         </div>
       </div>
