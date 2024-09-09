2a134ff6f0bbcb2cbc87f9956f1eb10ecf60756d
Emmanuel-Ejeagha <nuel6700@gmail.com>
2024-09-09 23:12:28 +0100

    modified

--- app/services/apiService.ts
+++ app/services/apiService.ts
@@ -1,78 +1,80 @@
 import { getAccessToken } from "../lib/actions";
 
 const apiService = {
-  get: async function (url: string): Promise<any> {
-    console.log("get", url);
+    get: async function (url: string): Promise<any> {
+        console.log('get', url);
 
-    const token = await getAccessToken();
+        const token = await getAccessToken();
 
-    return new Promise((resolve, reject) => {
-      fetch(`${process.env.NEXT_PUBLIC_API_HOST}${url}`, {
-        method: "GET",
-        headers: {
-          Accept: "application/json",
-          "Content-Type": "application/json",
-          Authorization: `Bearer ${token}`,
-        },
-      })
-        .then((response) => response.json()) // Correctly closing the arrow function
-        .then((json) => {
-          console.log("Response:", json);
-          resolve(json);
+        return new Promise((resolve, reject) => {
+            fetch(`${process.env.NEXT_PUBLIC_API_HOST}${url}`, {
+                method: 'GET',
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json',
+                    'Authorization': `Bearer ${token}`
+                }
+            })
+                .then(response => response.json())
+                .then((json) => {
+                    console.log('Response:', json);
+
+                    resolve(json);
+                })
+                .catch((error => {
+                    reject(error);
+                }))
         })
-        .catch((error) => {
-          // Correctly closing the arrow function
-          reject(error);
-        });
-    });
-  },
+    },
 
-  post: async function (url: string, data: any): Promise<any> {
-    console.log("post", url, data);
-    const token = await getAccessToken();
+    post: async function(url: string, data: any): Promise<any> {
+        console.log('post', url, data);
 
-    return new Promise((resolve, reject) => {
-      fetch(`${process.env.NEXT_PUBLIC_API_HOST}${url}`, {
-        method: "POST",
-        body: data,
-        headers: {
-          Authorization: `Bearer ${token}`,
-        },
-      })
-        .then((response) => response.json())
-        .then((json) => {
-          console.log("Response:", json);
-          resolve(json);
+        const token = await getAccessToken();
+
+        return new Promise((resolve, reject) => {
+            fetch(`${process.env.NEXT_PUBLIC_API_HOST}${url}`, {
+                method: 'POST',
+                body: data,
+                headers: {
+                    'Authorization': `Bearer ${token}`
+                }
+            })
+                .then(response => response.json())
+                .then((json) => {
+                    console.log('Response:', json);
+
+                    resolve(json);
+                })
+                .catch((error => {
+                    reject(error);
+                }))
         })
-        .catch((error) => {
-          // Correctly closing the arrow function
-          reject(error);
-        });
-    });
-  },
-  postWithoutToken: async function (url: string, data: any): Promise<any> {
-    console.log("post", url, data);
-    console.log("Post");
-    return new Promise((resolve, reject) => {
-      fetch(`${process.env.NEXT_PUBLIC_API_HOST}${url}`, {
-        method: "POST",
-        body: data,
-        headers: {
-          Accept: "application/json",
-          "Content-Type": "application/json",
-        },
-      })
-        .then((response) => response.json())
-        .then((json) => {
-          console.log("Response:", json);
-          resolve(json);
+    },
+
+    postWithoutToken: async function(url: string, data: any): Promise<any> {
+        console.log('post', url, data);
+
+        return new Promise((resolve, reject) => {
+            fetch(`${process.env.NEXT_PUBLIC_API_HOST}${url}`, {
+                method: 'POST',
+                body: data,
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json'
+                }
+            })
+                .then(response => response.json())
+                .then((json) => {
+                    console.log('Response:', json);
+
+                    resolve(json);
+                })
+                .catch((error => {
+                    reject(error);
+                }))
         })
-        .catch((error) => {
-          // Correctly closing the arrow function
-          reject(error);
-        });
-    });
-  },
-};
+    }
+}
 
 export default apiService;
