f102c7d921f94bdf2ee2a888755ca879bd79bcdd
Emmanuel-Ejeagha <nuel6700@gmail.com>
2024-09-09 23:13:23 +0100

    modified

--- app/components/forms/CustomButton.tsx
+++ app/components/forms/CustomButton.tsx
@@ -1,23 +1,22 @@
-// The custom button setup
 interface CustomButtonProps {
-  label: string;
-  className?: string;
-  onClick?: () => void;
+    label: string;
+    className?: string;
+    onClick: () => void;
 }
 
 const CustomButton: React.FC<CustomButtonProps> = ({
-  label,
-  className,
-  onClick,
+    label,
+    className,
+    onClick
 }) => {
-  return (
-    <div
-      onClick={onClick}
-      className={`w-full py-4 text-center bg-airbnb hover:bg-airbnb-dark text-white rounded-xl transition cursor-pointer ${className}`}
-    >
-      {label}
-    </div>
-  );
-};
+    return (
+        <div 
+            onClick={onClick}
+            className={`w-full py-4 bg-airbnb hover:bg-airbnb-dark text-white text-center rounded-xl transition cursor-pointer ${className}`}
+        >
+            {label}
+        </div>
+    )
+}
 
 export default CustomButton;
