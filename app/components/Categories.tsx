26618118bf09c1ce36127741c27a7a3750dc66b8
Emmanuel-Ejeagha <nuel6700@gmail.com>
2024-09-09 23:01:11 +0100

    Added: Categories

--- app/components/Categories.tsx
+++ app/components/Categories.tsx
@@ -1,30 +1,98 @@
+"use client";
+
+import { useState } from "react";
 import Image from "next/image";
+import useSearchModal, { SearchQuery } from "../hooks/useSearchModal";
 
 const Categories = () => {
+  const searchModal = useSearchModal();
+  const [category, setCategory] = useState("");
+
+  const _setCategory = (_category: string) => {
+    setCategory(_category);
+
+    const query: SearchQuery = {
+      country: searchModal.query.country,
+      checkIn: searchModal.query.checkIn,
+      checkOut: searchModal.query.checkOut,
+      guests: searchModal.query.guests,
+      bedrooms: searchModal.query.bedrooms,
+      bathrooms: searchModal.query.bathrooms,
+      category: _category,
+    };
+
+    searchModal.setQuery(query);
+  };
+
   return (
     <div className="pt-3 cursor-pointer pb-6 flex items-center space-x-12">
-      <div className="pb-4 flex flex-col items-center space-y-2 border-b-2 border-black opacity-60 hover:opacity-100 hover:border-gray-200">
-        <Image src="/icons.png" alt="icon logo" width={20} height={20} />
-        <span className="text-xs">Icons</span>
+      <div
+        onClick={() => _setCategory("")}
+        className={`pb-4 flex flex-col items-center space-y-2 border-b-2 ${
+          category == "" ? "border-black" : "border-white"
+        } opacity-60 hover:border-gray-200 hover:opacity-100`}
+      >
+        <Image src="/icons.png" alt="Category - Beach" width={20} height={20} />
+
+        <span className="text-xs">All</span>
       </div>
-      <div className="pb-4 flex flex-col items-center space-y-2 border-b-2 border-white opacity-60 hover:opacity-100 hover:border-gray-200">
-        <Image src="/island.jpg" alt="icon logo" width={20} height={20} />
-        <span className="text-xs">Island</span>
-      </div>
-      <div className="pb-4 flex flex-col items-center space-y-2 border-b-2 border-white opacity-60 hover:opacity-100 hover:border-gray-200">
-        <Image src="/beach.jpg" alt="icon logo" width={20} height={20} />
+
+      <div
+        onClick={() => _setCategory("beach")}
+        className={`pb-4 flex flex-col items-center space-y-2 border-b-2 ${
+          category == "beach" ? "border-black" : "border-white"
+        } opacity-60 hover:border-gray-200 hover:opacity-100`}
+      >
+        <Image src="/beach.jpg" alt="Category - Beach" width={20} height={20} />
+
         <span className="text-xs">Beach</span>
       </div>
-      <div className="pb-4 flex flex-col items-center space-y-2 border-b-2 border-white opacity-60 hover:opacity-100 hover:border-gray-200">
-        <Image src="/boathouse.jpg" alt="icon logo" width={20} height={20} />
-        <span className="text-xs">Houseboats</span>
+
+      <div
+        onClick={() => _setCategory("villas")}
+        className={`pb-4 flex flex-col items-center space-y-2 border-b-2 ${
+          category == "villas" ? "border-black" : "border-white"
+        } opacity-60 hover:border-gray-200 hover:opacity-100`}
+      >
+        <Image
+          src="/island.jpg"
+          alt="Category - Beach"
+          width={20}
+          height={20}
+        />
+
+        <span className="text-xs">Villas</span>
       </div>
-      <div className="pb-4 flex flex-col items-center space-y-2 border-b-2 border-white opacity-60 hover:opacity-100 hover:border-gray-200">
-        <Image src="/cabins.jpg" alt="icon logo" width={20} height={20} />
+
+      <div
+        onClick={() => _setCategory("cabins")}
+        className={`pb-4 flex flex-col items-center space-y-2 border-b-2 ${
+          category == "cabins" ? "border-black" : "border-white"
+        } opacity-60 hover:border-gray-200 hover:opacity-100`}
+      >
+        <Image
+          src="/cabins.jpg"
+          alt="Category - Beach"
+          width={20}
+          height={20}
+        />
+
         <span className="text-xs">Cabins</span>
       </div>
-      <div className="pb-4 flex flex-col items-center space-y-2 border-b-2 border-white opacity-60 hover:opacity-100 hover:border-gray-200">
-        <Image src="/tinyhomes.jpg" alt="icon logo" width={20} height={20} />
+
+      <div
+        onClick={() => _setCategory("tiny_homes")}
+        className={`pb-4 flex flex-col items-center space-y-2 border-b-2 ${
+          category == "tiny_homes" ? "border-black" : "border-white"
+        } opacity-60 hover:border-gray-200 hover:opacity-100`}
+      >
+        <Image
+          src="/tinyhomes.jpg"
+          alt="Category - Beach"
+          width={20}
+          height={20}
+        />
+
         <span className="text-xs">Tiny homes</span>
       </div>
     </div>
